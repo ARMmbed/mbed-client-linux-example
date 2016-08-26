8e256272dbedbff257c824d26cf766f9f9633cc6
Tero Jääskö <tero.jaasko@arm.com>
2016-08-26 18:56:56 +0300

    Make the makefile able to compile the result binary again

---
+++ include_dirs.mk
@@ -0,0 +1,10 @@
+INCLUDE_DIRS := \
+	../ \
+	. \
+	source \
+	mbed-client \
+	mbed-client-c \
+	nanostack-libservice/mbed-client-libservice \
+	mbed-trace \
+
+
---
+++ sources.mk
@@ -0,0 +1,6 @@
+SRCS += \
+	source/main.cpp \
+
+
+
+
